5021d75ccaa0cf6d9862b7c0b3e898c1a3b4c71a
Elizabeth Silvia Pumacota Quispe <elizabethsilvia.15@gmail.com>
2022-09-23 20:41:57 -0500

    Se agregó Actualizar y Eliminar/Finalizado

--- src/main/java/pe/isil/ep1/Application.java
+++ src/main/java/pe/isil/ep1/Application.java
@@ -20,25 +20,9 @@ public class Application {
                 "root",
                 "root"
         );
-/*
-        //Create Statement LISTAR REGISTROS
-        Statement st = connexion.createStatement();
 
-        ResultSet listar = st.executeQuery("select * from Movie");
 
-        while (listar.next()){
-            System.out.println(listar.getString("title")+"\n"+
-                    "Director: "+listar.getString("director")+"\n"+
-                    "Protagonistas: "+listar.getString("stars")+"\n"+
-                    "Música: "+listar.getString("music")+"\n"+
-                    "Duración: "+listar.getString("runTime")+" minutos"+"\n"+
-                    "País: "+listar.getString("country")+"\n"+
-                    "Género: "+listar.getString("filmGenre")+"\n"+
-                    "Sinopsis: "+listar.getString("plot")+"\n");
-        }
-*/
-
-        //Create Statement INSERTAR REGISTROS
+        // INSERTAR REGISTROS
 
         PreparedStatement preparedStatement = connexion.prepareStatement("insert into Movie values(?,?,?,?,?,?,?,?,?)");
         preparedStatement.setInt(1,12);
@@ -56,11 +40,77 @@ public class Application {
         System.out.println("Filas afectadas: "+filasafectadas);
 
         //Search item
+        Statement st2= connexion.createStatement();
+        ResultSet newInsert = st2.executeQuery("select * from movie where id='13'");
+
+        while (newInsert.next()){
+
+            System.out.println("REGISTRO INSERTADO"+"\n"+newInsert.getString("title")+"\n"+
+                    "Director: "+newInsert.getString("director")+"\n"+
+                    "Protagonistas: "+newInsert.getString("stars")+"\n"+
+                    "Música: "+newInsert.getString("music")+"\n"+
+                    "Duración: "+newInsert.getString("runTime")+" minutos"+"\n"+
+                    "País: "+newInsert.getString("country")+"\n"+
+                    "Género: "+newInsert.getString("filmGenre")+"\n"+
+                    "Sinopsis: "+newInsert.getString("plot")+"\n");
+        }
+
+
+        // LISTAR REGISTROS
+        Statement st = connexion.createStatement();
+
+        ResultSet listar = st.executeQuery("select * from Movie");
+
+        while (listar.next()){
+            System.out.println("LISTA DE PELICULAS"+"\n"+listar.getString("title")+"\n"+
+                    "Director: "+listar.getString("director")+"\n"+
+                    "Protagonistas: "+listar.getString("stars")+"\n"+
+                    "Música: "+listar.getString("music")+"\n"+
+                    "Duración: "+listar.getString("runTime")+" minutos"+"\n"+
+                    "País: "+listar.getString("country")+"\n"+
+                    "Género: "+listar.getString("filmGenre")+"\n"+
+                    "Sinopsis: "+listar.getString("plot")+"\n");
+        }
 
 
 
 
+        //ACTUALIZAR REGISTROS
+        CallableStatement callActualizar = connexion.prepareCall("{call actualizar(?,?)}");
+        callActualizar.setInt(1,1);
+        callActualizar.setString(2,"Drama Historico,Romance");
 
+        ResultSet resultSetSP1 = callActualizar.executeQuery();
+
+        int filasafectadas2= callActualizar.executeUpdate();
+        System.out.println("Filas afectadas: "+filasafectadas2);
+
+        //Search item
+        PreparedStatement updateitem =
+                connexion.prepareStatement("select * from movie where id=?");
+
+        updateitem.setInt(1,1);
+
+        ResultSet newIntem2 = updateitem.executeQuery();
+
+        while (newIntem2.next()){
+            System.out.println("REGISTRO ACTUALIZADO"+"\n"+newIntem2.getString("title")+"\n"+
+                    "Género: "+newIntem2.getString("filmGenre")+"\n");
+        }
+
+
+        //ELIMINAR REGISTRO
+
+        CallableStatement callEliminar = connexion.prepareCall("{call eliminar(?)}");
+        callEliminar.setInt(1,13);
+
+        ResultSet resultSetSP2 = callEliminar.executeQuery();
+
+        System.out.println("REGISTRO ELIMINADO");
+
+//CERRAR
+
+        connexion.close();
 
     }
 
